c02d12aa4d8f9afaff0d92dc689943767c5f8d4c
mahady13 <mohiuddinmahady@gmail.com>
2026-07-07 04:38:50 +0600

    added the model loading function

--- main.py
+++ main.py
@@ -7,3 +7,15 @@ from model_architectures import MyModel
 from torchvision import transforms
 from PIL import Image
 from sklearn.preprocessing import LabelEncoder
+
+device=torch.device("cuda" if torch.cuda.is_available() else "cpu")
+def load_asset():
+    model=MyModel()
+    model.load_state_dict(torch.load("animal_classifier.pth",map_location=device))
+    model.to(device)
+    model.eval()
+
+    label_encoder=joblib.load("label_encoder.pkl")
+    return model,label_encoder
+
+model,label_encoder=load_asset()
